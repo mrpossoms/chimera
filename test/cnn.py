a03c5ffe7affb9c8c5f9cc67c8e9187ca2118cdf
Kirk Roerig <mr.possoms@gmail.com>
2017-01-21 12:14:18 +0000

    Taking architecture inspiration from Krizhevsky Sutskever and Hinton

--- test/cnn.py
+++ test/cnn.py
@@ -8,197 +8,9 @@ import tensorflow as tf
 import numpy as np
 import struct
 import os
+from util import *
 
 old_cwd = os.getcwd()
-
-
-class FileTrainingSet():
-    def __init__(self, base_path):
-        self.sample_paths = os.listdir(base_path)
-        self.index = 0
-        self.cache = {}
-
-        # remove hidden files
-        for file in self.sample_paths:
-            if file[0] == '.':
-                self.sample_paths.remove(file)
-
-        os.chdir(base_path)
-
-    def reset(self):
-        self.index = 0
-
-    def next_batch(self, size, decoder=tf.image.decode_png):
-        images, labels = [], []
-
-        batch = self.sample_paths[self.index:self.index + size]
-        self.index += size
-
-        # coord = tf.train.Coordinator()
-        # threads = tf.train.start_queue_runners(coord=coord)
-
-        for path in batch:
-            print(path)
-            name_and_label = path.split('.')[0].split('-')
-
-            image = None
-            if path in self.cache:
-               image = self.cache[path] 
-            else:
-               file_content = tf.read_file(path)
-               image = tf.image.rgb_to_grayscale(decoder(file_content)) #tf.to_float(decoder(file_content)) / 255.  # use png or jpg decoder based on your files.
-               image = tf.image.resize_images([image], [112, 112]).eval().flatten() / 255.0
-               self.cache[path] = image
-
-            images += [image]
-
-            if name_and_label[1] is '1':
-                labels += [1.0]
-            else:
-                labels += [-1.0]
-
-        # coord.request_stop()
-        # coord.join(threads)
-
-        return np.asarray(images), np.asarray(labels).reshape([len(labels), 1])
-
-
-class BlobTrainingSet():
-    def __init__(self, path):
-        self.index = 0
-        self.file = open(path, mode='rb')
-
-    def reset(self):
-        self.index = 0
-        self.file.seek(0);
-
-    def size(self):
-        last_pos = self.file.tell()
-        self.file.seek(0, 2)
-        size = self.file.tell() // (4 + 112 ** 2)
-        self.file.seek(last_pos, 0)
-        return size
-
-    def next_batch(self, size):
-        images, labels = [], []
-
-
-        # coord = tf.train.Coordinator()
-        # threads = tf.train.start_queue_runners(coord=coord)
-
-        for _ in range(size):
-            buf = self.file.read(112**2)
-            tag = struct.unpack('I', self.file.read(4))[0]
-
-            assert tag is 0 or tag is 1
-
-            images += [ np.frombuffer(buf, dtype=np.uint8).reshape(112**2) / 255.0 ]
-            # Image.frombytes('L', (128, 128), buf).show()
-            if tag == 1:
-                labels += [1.0]
-            else:
-                labels += [-1.0]
-
-        self.index += size
-        # coord.request_stop()
-        # coord.join(threads)
-
-        return np.asarray(images), np.asarray(labels).reshape([len(labels), 1])
-        #return np.asarray(images), np.asarray(labels)
-
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-
-
-def bias_variable(shape):
-  #initial = tf.constant(0.1, shape=shape)
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    # strides
-    #   [0] - batch
-    #   [1] - width
-    #   [2] - height
-    #   [3] - channels
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-  # max pooling here is sliding a 2x2 filter across the activation
-  # map, and selecting the max activation as the value to be used
-  # for the downsampled pool. This effectively divides the resolution
-  # of the activation map by two
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
-
-
-def shape_str(np_mat):
-    shape = ""
-    for dim in np_mat.shape:
-        shape += '-%d' % dim
-
-    return shape
-
-
-def shape_from_str(str):
-    shape = []
-    str = str.replace('b-', '').replace('w-', '').split('-')
-
-    for size in str:
-        shape.append(int(size))
-
-    return shape
-
-
-def load_layer(layer_path, w_tensor, b_tensor):
-    weights, biases = None, None
-
-    for file in os.listdir(layer_path):
-        if file[0] is not 'b' and file[0] is not 'w':
-            continue
-
-        shape = shape_from_str(file)
-        mat = np.fromfile('%s/%s' % (layer_path, file), dtype=float).reshape(shape)
-        if file[0] is 'b':
-            biases = mat
-            print(mat)
-        elif file[0] is 'w':
-            weights = mat
-
-    return w_tensor.assign(weights), b_tensor.assign(biases)
-
-
-def save_layer(layer_path, w_tensor=None, b_tensor=None):
-    # make sure the la
-    try:
-        os.mkdir(layer_path)
-    except FileExistsError:
-        pass
-
-    if w_tensor is not None:
-        mat = w_tensor.eval().astype(float)
-        mat.tofile('%s/w%s' % (layer_path, shape_str(mat)))
-
-    if b_tensor is not None:
-        mat = b_tensor.eval().astype(float)
-        mat.tofile('%s/b%s' % (layer_path, shape_str(mat)))
-
-
-def save_image(path, array):
-    bitmap = (array * 255).astype(np.uint8)
-    img = Image.fromarray(bitmap, mode='L')
-    img.save(path + '.png')
-
-
-def save_activation_map(path, tensor, name):
-    act_maps = np.dsplit(tensor[0], tensor.shape[3])
-    filter_idx = 0
-    for act_map in act_maps:
-        save_image(path + '/' + str(filter_idx), act_map.reshape(act_map.shape[:2]))
-        filter_idx += 1
 
 def test(y_conv, y_, accuracy, h_pool4):
     final_acc = 0
@@ -241,38 +53,38 @@ with tf.Session() as sess:
     # that this dimension will be equal to that of the number of training
     # samples that are part of the selected batch
     x_image = max_pool_2x2(tf.reshape(x, [-1,width,height,1]))
-    s_conv1 = [5, 5, 1, 16]
+    s_conv1 = [5, 5, 1, 48]
     W_conv1 = weight_variable(s_conv1) # 5 x 5 x 1 kernels 32 deep?
     b_conv1 = bias_variable([s_conv1[3]]) # A bias for each kernel neuron
 
     # slides filter across image, run through the ReLU activation
     # function, kernel bias is also applied
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1, strides=[1, 3, 3, 1]) + b_conv1) # 36 x 36
     #h_pool1 = tf.nn.l2_normalize((h_conv1), [1, 2]) # 56 X 56
-    h_pool1 = tf.nn.l2_normalize(max_pool_2x2(h_conv1), [1, 2]) # 56 X 56
+    #h_pool1 = tf.nn.l2_normalize(max_pool_2x2(h_conv1), [1, 2]) # 56 X 56
     #h_pool1 = max_pool_2x2(h_conv1) # 56 X 56
     #---------------------------------------------------------------------------
-    s_conv2 = [5, 5, 16, 8]
+    s_conv2 = [3, 3, 48, 96]
     W_conv2 = weight_variable(s_conv2)
     b_conv2 = bias_variable([s_conv2[3]])
 
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
     #h_pool2 = tf.nn.l2_normalize((h_conv2), [1, 2]) # 28 x 28
-    h_pool2 = tf.nn.l2_normalize(max_pool_2x2(h_conv2), [1, 2]) # 28 x 28
+    h_pool2 = tf.nn.l2_normalize((h_conv2), [1, 2]) # 34 x 34
     #h_pool2 = max_pool_2x2(h_conv2) # 28 x 28
     #---------------------------------------------------------------------------
-    s_conv4 = [5, 5, 8, 8]
+    s_conv4 = [5, 5, 96, 128]
     W_conv4 = weight_variable(s_conv4)
     b_conv4 = bias_variable([s_conv4[3]])
 
-    h_conv4 = tf.nn.relu(conv2d(h_pool2, W_conv4) + b_conv4)
+    h_conv4 = tf.nn.relu(conv2d(h_pool2, W_conv4, strides=[1, 3, 3, 1]) + b_conv4)
     #h_pool4 = tf.nn.l2_normalize((h_conv4), [1, 2]) # 7 x 7
-    h_pool4 = tf.nn.l2_normalize(max_pool_2x2(h_conv4), [1, 2]) # 7 x 7
+    h_pool4 = tf.nn.l2_normalize((h_conv4), [1, 2]) # 10 x 10
     #h_pool4 = max_pool_2x2(h_conv4)# 7 x 7
     #h_pool4 = h_conv4# 7 x 7
 
     keep_prob = tf.placeholder(tf.float32)
-    hp4_width, hp4_height = 7, 7 #h_conv4.shape[1] / 2, h_conv4.shape[2] / 2
+    hp4_width, hp4_height = 10, 10 #h_conv4.shape[1] / 2, h_conv4.shape[2] / 2
     # hp4_width, hp4_height = 32, 32
     W_fc1 = weight_variable([hp4_width * hp4_height * s_conv4[3], 512])
     b_fc1 = bias_variable([512])
